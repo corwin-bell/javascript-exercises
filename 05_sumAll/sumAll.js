c63da1dcccbce650e0ead1d2bddc7de58f281327
corwin-bell <corwin.m.bell@gmail.com>
2023-12-07 15:01:05 -0700

    initial function to pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(minInt,maxInt) {
+let intSum = 0;
+for (let i = minInt; i <= maxInt; i++) {
+    intSum += i;
+}
+return intSum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;

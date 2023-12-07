eec91bdd4eec56104b2cc9a48d636b894fecc219
corwin-bell <corwin.m.bell@gmail.com>
2023-12-07 11:10:26 -0700

    passed first test, failed second

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+// accepts variable number and type of arguments
+const removeFromArray = function(array, ...args) {
+    // only remove items that are present in the array
+    for (let arg of args) {
+        if (arg in array) {
+            let index = array.indexOf(arg);
+            array.splice(index,1);
+        }
+        else {
+            continue;
+        }
+    return array;
+    } 
+    // return the array
 
 };
 

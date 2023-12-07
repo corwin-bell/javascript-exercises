209844cb8d414406289df718e7784f8baed2da96
corwin-bell <corwin.m.bell@gmail.com>
2023-12-07 10:26:31 -0700

    passed first test for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatNum) {
+    let reString = string;
+    for (let i = 1; i <repeatNum; i++) {
+        reString += string;
+    }
+    return reString;
 };
 
 // Do not edit below this line

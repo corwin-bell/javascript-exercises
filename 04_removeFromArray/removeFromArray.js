3b2a34d13aa1a6da6882cbb0071e4ed3ce9bbf83
corwin-bell <corwin.m.bell@gmail.com>
2023-12-07 13:20:09 -0700

    replaced in operator with .includes() array method to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,13 @@
-// accepts variable number and type of arguments
 const removeFromArray = function(array, ...args) {
-    // only remove items that are present in the array
     for (let arg of args) {
-        if (arg in array) {
-            let index = array.indexOf(arg);
-            array.splice(index,1);
+        if (array.includes(arg)) {
+            array.splice(array.indexOf(arg),1);
         }
         else {
             continue;
         }
-    return array;
-    } 
-    // return the array
-
+    }
+    return array; 
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;

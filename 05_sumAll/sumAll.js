2f88869fa744c474eda543fe8a29ca047b076f0d
corwin-bell <corwin.m.bell@gmail.com>
2023-12-07 15:30:01 -0700

    my solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,18 @@
-const sumAll = function(minInt,maxInt) {
-let intSum = 0;
-for (let i = minInt; i <= maxInt; i++) {
-    intSum += i;
-}
-return intSum;
+const sumAll = function(int1,int2) {
+    if (int1 < 0 || 
+        int2 < 0 || 
+        typeof int1 !== 'number' || 
+        typeof int2 !== 'number'
+        ) {
+        return 'ERROR';
+    }
+    intArray = [int1,int2].sort((a, b) => a - b);
+    intArray
+    let intSum = 0;
+    for (let i = intArray[0]; i <= intArray[1]; i++) {
+        intSum += i;
+    }
+    return intSum;
 };
 // Do not edit below this line
 module.exports = sumAll;

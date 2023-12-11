d7fe9a1a7e4420fd798e20e8e654860f179de863
corwin-bell <corwin.m.bell@gmail.com>
2023-12-11 16:47:45 -0700

    passed all F to C tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,14 @@
-const convertToCelsius = function() {
+// formula of temp from Fahrenheit to Celsius
+// tempC = (tempF - 32) * 5/9
+// round to 1 decimal place
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5/9;
+  let tempCrounded = Math.round(tempC * 10) / 10;
+  return tempCrounded;
 };
 
+// formula of temp from Celsius to Fahrenheit
+// tempF = 9/5 * tempC + 32 
 const convertToFahrenheit = function() {
 };
 

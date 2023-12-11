326c2d491faf65ef22d4d535514879857f13d115
corwin-bell <corwin.m.bell@gmail.com>
2023-12-11 16:25:11 -0700

    initial solution that passes first 2 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+// determine whether a given year is a leap year
+// years divisible by 400 are always leap years
+// otherwise, years divisible by 4 and not divisible by 100 are leap years
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true;
+    }
+    else if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+    else {return false;}
 };
 
 // Do not edit below this line

87d17176b01828f73336ba013a2402967e32a86f
corwin-bell <corwin.m.bell@gmail.com>
2023-12-21 16:10:58 -0700

    more concise solution, also more performant

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,23 +1,24 @@
-const fibonacci = function(number) {
-    if (number < 0) return 'OOPS';
-    let fibNumber1 = 1;
-    let fibNumber2 = 1;
-    let fibNumber3 = 1;
-    if (number >= 1 && number <= 2) {
-        return fibNumber3;
+const fibonacci = function(count) {
+    if (count < 0) return 'OOPS';
+    if (count === 0) return 0;
+    let fibcount1 = 1;
+    let fibcount2 = 1;
+    if (count > 2) {
+        for (let i = 3; i <=count; i++) {
+            let fibcount3 = fibcount1 + fibcount2;
+            fibcount1 = fibcount2;
+            fibcount2 = fibcount3;
+        }
     }
-    for (let i = 3; i <=number; i++) {
-        fibNumber3 = fibNumber1 + fibNumber2;
-        fibNumber1 = fibNumber2;
-        fibNumber2 = fibNumber3;
-    }
-    return fibNumber3;
+    return fibcount2;
 };
 
 // how to test performance
 // const start = performance.now();
 // await fibonacci(5);
 // const end = performance.now();
+// const executionTime = end - start;
+// executionTime
 // console.log(`Execution time: ${end - start} ms`);
 
 // Do not edit below this line

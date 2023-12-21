2e6146bf4ea911a969404fb9226e7f08b92bf02a
corwin-bell <corwin.m.bell@gmail.com>
2023-12-20 22:11:28 -0700

    my solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.replace(/\W/g, '').toLowerCase();
+    return cleanString === [...cleanString].reverse().join("");
 };
 
 // Do not edit below this line

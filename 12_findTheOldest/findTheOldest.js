4304c9d7c6b7cd21d029eb73a0865e4a29390a12
corwin-bell <corwin.m.bell@gmail.com>
2023-12-21 17:42:50 -0700

    my solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    peopleArray.forEach((person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    const ageArray = peopleArray.map(person => person.yearOfDeath - person.yearOfBirth);
+    const maxAge = Math.max(...ageArray);
+    const oldestPerson = peopleArray[ageArray.indexOf(maxAge)];
+    return oldestPerson;
 };
 
 // Do not edit below this line
